57020351c0d240d5edb93653773f7a1e52fa065a
shabbir3g <mostafizshabbir@gmail.com>
2018-05-05 16:13:39 +0600

    work on quote form

--- footer.php
+++ footer.php
@@ -62,12 +62,9 @@
 		<div class="modal fade show" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="false">
 		  <div class="modal-dialog" role="document">
 			<div class="modal-content">
-			  <div class="modal-header">
-				<h5 class="modal-title" id="exampleModalLabel">Quote Form</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 				  <span aria-hidden="true">&times;</span>
 				</button>
-			  </div>
 			  <div class="modal-body">
 			  
 			  
